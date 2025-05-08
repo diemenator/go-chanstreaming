928bc35fd47dc5ddd4bcbd1c64b5d373bd5146df
Sergey Olontsev <sergey@olontsev.io>
2025-05-08 09:51:32 +0100

    *

--- tests/timing_test.go
+++ tests/timing_test.go
@@ -30,36 +30,20 @@ func TestThrottle(t *testing.T) {
 
 func TestJitter(t *testing.T) {
 	source := make(chan int)
-	n := 100
 	go func() {
 		defer close(source)
-		for i := range n {
+		for i := range 5 {
 			source <- i
 		}
 	}()
 
-	// r := rand.New(rand.NewSource(time.Now().UnixNano()))
-	// r = rand.New(rand.NewSource(1))
 	throttle := ch.Jitter[int](10 * time.Millisecond)
 	out := throttle(source)
 
-	count := []int{0, 0, 0}
-
-	for i := range n {
-		start := time.Now()
-		v := <-out
-		elapsed := time.Since(start)
-		assert.Equal(t, i, v)
-		if elapsed < 1*time.Millisecond {
-			count[0]++
-		} else if elapsed < 6*time.Millisecond {
-			count[1]++
-		} else {
-			count[2]++
-		}
-	}
-
-	assert.GreaterOrEqual(t, count[0], n/2)
-	assert.GreaterOrEqual(t, count[1], n/3)
-	assert.Equal(t, 0, count[2])
+	// TODO: it's better to test the histogram of delays
+	start := time.Now()
+	result := ch.ToSlice(out)
+	elapsed := time.Since(start)
+	assert.Equal(t, []int{0, 1, 2, 3, 4}, result)
+	assert.LessOrEqual(t, elapsed, 50*time.Millisecond)
 }
